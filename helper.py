af674a5148430ba2bc3c3376820fceaa2397e57c
cedric <wenqipei@foxmail.com>
2025-08-13 20:22:08 +0800

    update on sample experiment

--- helper.py
+++ helper.py
@@ -99,7 +99,7 @@ def write_result_to_file(question, pred_sql, usefulness_score, eval_path, output
     result = {
         "question_id": question["question_id"], 
         "question": question["question"], 
-        # "evidence": question["evidence"],
+        "evidence": question["evidence"],
         "gold_sql": question["gold_sql"],
         "predicted_sql": pred_sql, 
         "ex": question["ex"],
