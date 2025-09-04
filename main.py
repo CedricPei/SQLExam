e80b1d16916447f54fe77afa7f672890cbdb552b
CedricPei <wenqi_pei@u.nus.edu>
2025-09-05 01:53:33 +0800

    add prover and refuter

--- main.py
+++ main.py
@@ -2,39 +2,41 @@ import json
 from Pipe import SQLEvaluationPipeline
 from helper import execute_sql, write_result_to_file, run_with_timeout, compare_result
 from tqdm import tqdm
+from Prover import Prover
+from Refuter import Refuter
 
-model = "deepseek-chat"
+
+reasoning_model = "deepseek-reasoner"
+instruct_model = "deepseek-chat"
 partial = True
+
+Prover = Prover(model=reasoning_model)
+Refuter = Refuter(model=reasoning_model)
+PartialEval = SQLEvaluationPipeline(model=instruct_model)
 
 if __name__ == "__main__":
     with open("samples_20.json", "r", encoding="utf-8") as f:
         questions = json.load(f)
-    # questions = [q for q in questions if q["question_id"] == 1481]
-
-    Prover = Prover(model=model)
-    Refuter = Refuter(model=model)
-    PartialEval = SQLEvaluationPipeline(model=model)
     
     for question in tqdm(questions):
         pred_sql = question["predicted_sql"]
         db_id = question["db_id"]
         gold_sql = question["gold_sql"]
 
-
         pred_res = run_with_timeout(execute_sql, db_id, pred_sql, timeout=20)
         gold_res = run_with_timeout(execute_sql, db_id, gold_sql, timeout=20)
 
-        if pred_res is None and gold_res is not None:
-            score = 0.0
-        elif compare_result(pred_res, gold_res):
-            score = 1.0 if not Refuter.call(question, pred_sql) else 0.0
+        score = 0.0
+        if compare_result(pred_res, gold_res):
+            refuter_res = Refuter.call(question, pred_sql, pred_res, gold_res)
+            score = 1.0 if not refuter_res else 0.0
         else:
-            if Prover.call(question, pred_sql):
-                score = 1.0 if not Refuter.call(question, pred_sql) else 0.0
-            else:
-                score = 0.0
+            prover_res = Prover.call(question, pred_sql, pred_res)
+            if prover_res:
+                refuter_res = Refuter.call(question, pred_sql, pred_res, gold_res)
+                score = 1.0 if not refuter_res else 0.0
 
         if score != 1.0 and partial:
             score = PartialEval.eval(question, pred_sql)
 
-        write_result_to_file(question, pred_sql, score)
+        write_result_to_file(question, pred_sql, score, prover_res, refuter_res)
